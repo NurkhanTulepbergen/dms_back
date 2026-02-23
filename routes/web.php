337a9617c2a8f15a7efe087c77264b7bdacab385
NurkhanTulepbergen <n_tulepbergen@kbtu.kz>
2026-02-23 14:30:08 +0500

    a

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Log;
+
+Route::get('/_debug/logtest', function () {
+    Log::info('LOGTEST_OK', [
+        'auth_check' => auth('web')->check(),
+        'user_id' => auth('web')->id(),
+        'role' => optional(auth('web')->user())->role,
+        'session_id' => session()->getId(),
+        'cookie_names' => array_keys(request()->cookies->all()),
+    ]);
+
+    return response()->json(['ok' => true]);
+});
 
 Route::get('/', function () {
     return view('welcome');
