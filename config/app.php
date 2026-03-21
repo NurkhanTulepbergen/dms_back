65db34ca4d52f8938ee3c25338ae0778787cc5f9
NurkhanTulepbergen <n_tulepbergen@kbtu.kz>
2026-03-21 05:20:50 +0500

    Fix forced https for admin assets

--- config/app.php
+++ config/app.php
@@ -54,6 +54,7 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
     'frontend_url' => env('FRONTEND_URL', env('APP_URL', 'http://localhost')),
+    'force_https' => env('APP_FORCE_HTTPS', false),
 
     /*
     |--------------------------------------------------------------------------
